b694ffba7e53ad0071af02b50354432a4939f555
TPL777 <tl606878@gmail.com>
2023-09-11 21:23:15 -0300

    ex 12

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -47,4 +47,10 @@ autores.nome AS autor
 FROM livros
 INNER JOIN autores ON livros.autor_id = autores.id;
 
+SELECT alunos.nome AS aluno,
+matriculas.curso
+FROM alunos
+INNER JOIN matriculas ON alunos.id = matriculas.aluno_id;
+
+
 
