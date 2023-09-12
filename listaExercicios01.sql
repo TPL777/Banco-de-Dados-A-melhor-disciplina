169e948553de8caeb4a1ca945539cc9e0ab0c05a
TPL777 <tl606878@gmail.com>
2023-09-11 21:34:45 -0300

    ex 17

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -70,3 +70,9 @@ LEFT JOIN livros ON autores.id = livros.autor_id
 GROUP BY autores.nome
 ORDER BY quantidade_de_livros DESC
 LIMIT 1;
+
+SELECT produto, SUM(receita) AS total_da_receita
+FROM vendas
+GROUP BY produto
+ORDER BY total_da_receita ASC
+LIMIT 1;
