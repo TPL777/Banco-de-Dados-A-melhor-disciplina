3a7845eb4eb0eba18aaf9863ff9e5e747bc6ca70
TPL777 <tl606878@gmail.com>
2023-09-25 21:10:00 -0300

    ex 2

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -8,3 +8,15 @@ DELIMITER ;
 -- Para testar a stored procedure:
 CALL sp_ListarAutores();
 
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END//
+DELIMITER ;
+
+-- Para testar a stored procedure:
+CALL sp_LivrosPorCategoria('Romance');
