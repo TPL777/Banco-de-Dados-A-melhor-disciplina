c17d9c751271b7bec0d26dde1f6bdf8c48471a44
TPL777 <tl606878@gmail.com>
2023-09-25 21:25:36 -0300

    ex 5

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -47,3 +47,15 @@ DELIMITER ;
 -- Para testar a stored procedure:
 CALL sp_VerificarLivrosCategoria('Ficção Científica', @possuiLivros);
 SELECT @possuiLivros;
+
+DELIMITER //
+CREATE PROCEDURE sp_LivrosAteAno(IN ano INT)
+BEGIN
+    SELECT Titulo
+    FROM Livro
+    WHERE Ano_Publicacao <= ano;
+END//
+DELIMITER ;
+
+-- Para testar a stored procedure:
+CALL sp_LivrosAteAno(2010);
